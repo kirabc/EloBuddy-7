12da791b9f57b307520882bc27969064e62bbf45
Toyota7 <mitsoylas.mitsonaras3@gmail.com>
2016-09-07 12:10:54 +0300

    v1.3

--- T7Mordekaiser/Properties/AssemblyInfo.cs
+++ T7Mordekaiser/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(T7_Morde.Program.Version)]
-[assembly: AssemblyFileVersion(T7_Morde.Program.Version)]
+[assembly: AssemblyVersion("1.3")]
+[assembly: AssemblyFileVersion("1.3")]
